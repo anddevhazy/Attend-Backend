913e7334f41e3f04dec26c62dcc3308d550bd1a8
anddevhazy <haywhy2946@gmail.com>
2025-09-16 10:48:25 +0100

    refactor(app.js) added security libraries

--- src/app.js
+++ src/app.js
@@ -3,9 +3,15 @@ import studentRoutes from './routes/student.routes.js';
 import lecturerRoutes from './routes/lecturer.routes.js';
 import authRoutes from './routes/auth.routes.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
+import helmet from 'helmet';
+import cors from 'cors';
+import xss from 'xss-clean';
 
 const app = express();
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/student', studentRoutes);
