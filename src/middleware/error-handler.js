fddae2e5d71e6941d47efff9dfaa504f5544dbaa
anddevhazy <haywhy2946@gmail.com>
2025-09-20 08:51:16 +0100

    fix: the import of customApiError

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -1,8 +1,8 @@
-import { CustomAPIError } from '../errors/index.js';
+import { CustomApiError } from '../errors/index.js';
 import { StatusCodes } from 'http-status-codes';
 // eslint-disable-next-line no-unused-vars
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
+  if (err instanceof CustomApiError) {
     return res.status(err.statusCode).json({ msg: err.message });
   }
   return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
